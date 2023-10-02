ca2115d34ad5699f7224c02ea617deeb059f65cd
wewantthefunk <wewantthefunk@yahoo.com>
2023-10-02 16:04:23 -0400

    updates

--- taskbartest.bat
+++
@@ -1,7 +0,0 @@
-DEL /F /S /Q /A "%AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\Taskbar\*"
-
-REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\TaskBand /F
-
-taskkill /f /im explorer.exe
-
-start explorer.exe
