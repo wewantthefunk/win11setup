b3c84e84b57506701cbef625ebd36c7ea5d8be81
wewantthefunk <wewantthefunk@yahoo.com>
2023-10-04 21:15:58 -0400

    updates

--- win11setup.ps1
+++ win11setup.ps1
@@ -64,118 +64,118 @@ function Unpin-AllTaskbarItems {
     Write-Host "All pinned items have been removed from the taskbar."
 }
 
-function Set-SoundScheme {
-    param (
-        [string]$SchemeName
-    )
+function Make-Updates {
+    # Remove all pinned items from taskbar
+    Unpin-AllTaskbarItems
 
-    # Define the registry path for the sound scheme settings
-    $soundSchemesRegistryPath = "HKCU:\AppEvents\Schemes\Names"
+    # Move the start menu to the left
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $CustomValueName = "TaskbarAl"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
 
-    # Check if the specified sound scheme exists
-    if (Test-Path "$soundSchemesRegistryPath\$SchemeName") {
-        # Set the selected sound scheme as the current one
-        Set-ItemProperty -Path "HKCU:\AppEvents\Schemes" -Name "(Default)" -Value "$SchemeName"
+    # Show all file extensions
+    $CustomValueName = "HideFileExt"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
 
-        # Apply the changes to the sound scheme
-        [Microsoft.Win32.Registry]::CurrentUser.Flush()
+    # Disable diagnostic
 
-        Write-Host "Sound scheme set to: $SchemeName"
-    } else {
-        Write-Host "Error: Sound scheme '$SchemeName' not found."
-    }
+    $CustomRegistryPath = "HKLM:\Software\Policies\Microsoft\Windows\DataCollection"
+    $CustomValueName = "AllowTelemetry"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Disable Feedback & Diagnostics
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+    $CustomValueName = "DoNotUseDiagnosticData"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
+
+    # Disable Cortana
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+    $CustomValueName = "AllowCortana"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Disable Advertising ID
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo"
+    $CustomValueName = "DisabledByGroupPolicy"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
+
+    # Hide Search Box
+
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"
+    $CustomValueName = "SearchboxTaskbarMode"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Change to Dark Mode
+
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+    $CustomValueName = "SystemUsesLightTheme"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Change Apps to Dark Mode
+
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+    $CustomValueName = "AppsUseLightTheme"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Remove chat Icon
+
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $CustomValueName = "TaskbarMn"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Remove weather Icon
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Dsh"
+    $CustomValueName = "AllowNewsAndInterests"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Remove widgets
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $CustomValueName = "TaskbarDa"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+    # Turn off notification center
+
+    $CustomRegistryPath = "HKCU:\Software\Policies\Microsoft\Windows\Explorer"
+    $CustomValueName = "DisableNotificationCenter"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
+
+    # Turn off search highlights
+
+    $CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings"
+    $CustomValueName = "IsDynamicSearchBoxEnabled"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+
+
+    # Turn off Powershell 2.0
+
+    $CustomRegistryPath = "HKLM:\SOFTWARE\Microsoft\PowerShell\1\PowerShellEngine"
+    $CustomValueName = "PowerShellVersion"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData "2.0"
+
+    # Set Sound Scheme to "No Sound"
+
+    # Set the system sound profile to "No Sound"
+    $CustomRegistryPath = "HKCU:\AppEvents\Schemes"
+    $CustomValueName = ".Default"
+    Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData "No Sound"
 }
 
-# Remove all pinned items from taskbar
-Unpin-AllTaskbarItems
+# Get the current date and time as a formatted string
+$dateTime = Get-Date -Format "yyyyMMddHHmmss"
 
-# Call the function to disable system event sounds
-Set-SoundScheme -SchemeName ".None"
+# Create a system restore point with a timestamp in the description
+$restorePointDescription = "Restore Point $dateTime"
+Checkpoint-Computer -Description $restorePointDescription -RestorePointType "MODIFY_SETTINGS"
 
-# Move the start menu to the left
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$CustomValueName = "TaskbarAl"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
+# Verify if the restore point was created
+if ($?) {
+    Write-Host "System restore point '$restorePointDescription' created successfully."
+    Make-Updates
+} else {
+    Write-Host "Failed to create a system restore point. Updates Aborted"
+}
 
-# Show all file extensions
-$CustomValueName = "HideFileExt"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Disable diagnostic
-
-$CustomRegistryPath = "HKLM:\Software\Policies\Microsoft\Windows\DataCollection"
-$CustomValueName = "AllowTelemetry"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Disable Feedback & Diagnostics
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
-$CustomValueName = "DoNotUseDiagnosticData"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
-
-# Disable Cortana
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-$CustomValueName = "AllowCortana"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Disable Advertising ID
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo"
-$CustomValueName = "DisabledByGroupPolicy"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
-
-# Hide Search Box
-
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"
-$CustomValueName = "SearchboxTaskbarMode"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Change to Dark Mode
-
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$CustomValueName = "SystemUsesLightTheme"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Change Apps to Dark Mode
-
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$CustomValueName = "AppsUseLightTheme"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Remove chat Icon
-
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$CustomValueName = "TaskbarMn"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Remove weather Icon
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Dsh"
-$CustomValueName = "AllowNewsAndInterests"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Remove widgets
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$CustomValueName = "TaskbarDa"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-# Turn off notification center
-
-$CustomRegistryPath = "HKCU:\Software\Policies\Microsoft\Windows\Explorer"
-$CustomValueName = "DisableNotificationCenter"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 1
-
-# Turn off search highlights
-
-$CustomRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings"
-$CustomValueName = "IsDynamicSearchBoxEnabled"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData 0
-
-
-# Turn off Powershell 2.0
-
-$CustomRegistryPath = "HKLM:\SOFTWARE\Microsoft\PowerShell\1\PowerShellEngine"
-$CustomValueName = "PowerShellVersion"
-Set-RegistryValue -RegistryPath $CustomRegistryPath -ValueName $CustomValueName -ValueData "2.0"
